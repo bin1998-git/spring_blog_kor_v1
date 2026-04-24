62327c19ac4c2f54abea7e5c8db1b4f4974e3ea1
bin1998-git <jbhan0217@naver.com>
2026-04-24 11:07:15 +0900

    first commit

---
+++ src/main/java/com/tenco/blog/controller/BoardController.java
@@ -0,0 +1,81 @@
+package com.tenco.blog.controller;
+
+import com.tenco.blog.model.Board;
+import com.tenco.blog.repository.BoardNativeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Slf4j
+@Controller // IoC
+@RequiredArgsConstructor // DI
+public class BoardController {
+    // DI
+    private final BoardNativeRepository boardNativeRepository;
+
+    /**
+     * 게시글 작성 화면 요청
+     * @return 페이지 반환
+     * 주소설계 : http://localhost:8080/board/save-form
+     */
+    @GetMapping("/board/save-form")
+    public String saveForm() {
+
+        return "board/save-form";
+    }
+
+    /**
+     * 게시글 작성 기능 요청
+     * @return 페이지 반환
+     * 주소설계 : http://localhost:8080/board/save-form
+     */
+    @PostMapping("/board/save")
+    public String saveProc(
+            @RequestParam("username") String username,
+            @RequestParam("title") String title,
+            @RequestParam("content") String content) {
+        log.info("username : " + username);
+        log.info("title : " + title);
+        log.info("content : " + content);
+        // insert + 트랜잭션 처리
+        boardNativeRepository.save(title, content, username);
+        // redirect <-- 다시 URL 요청 해 !
+        //return "redirect:/";
+        return "redirect:/";
+    }
+
+
+    /**
+     * 게시글 목록 화면 요청
+     * 주소설계 : http://localhost:8080/
+     */
+    @GetMapping({"/", "index"})
+    public String list(Model model) {
+        //
+        List<Board> boardList = boardNativeRepository.findAll();
+        model.addAttribute("boardList", boardList);
+        return "board/list";
+    }
+
+
+    // 게시글 상세보기 화면 요청
+    // http://localhost:8080/board/{id}
+    @GetMapping("/board/{id}")
+    public String detailPage(@PathVariable(name = "id") Integer id, Model model) {
+        // 유효성 검사 , 인증 검사
+       Board board =  boardNativeRepository.findById(id);
+       model.addAttribute("board", board);
+
+
+        return "board/detail";
+    }
+
+
+}
